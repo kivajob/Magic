81d59e765cfe620b9cec562f40eecc6251dc6150
Button <button@localhost.localdomain>
2018-05-26 16:57:18 -0400

    learn_chef_httpd

---
+++ learn_chef_httpd/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook:: learn_chef_httpd
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'httpd'
+
+service 'httpd' do
+	action :start
+	start_command '/usr/sbin/httpd -k start'
+end
+
+
+template '/var/www/html/index.html' do
+	source 'index.html.erb'
+end
